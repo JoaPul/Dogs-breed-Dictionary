9e71a2ea84a06141042bf47ecc1f4fe6501cf405
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-05-19 18:07:41 -0500

    change in dog rout

--- src/routes/dogs/dog.jsx
+++ src/routes/dogs/dog.jsx
@@ -19,12 +19,12 @@ const Dog = () => {
 
   return (
     <>
-      <div style={{ height: "400px", width: "inherit", display: "flex", alignContent: "center", justifyContent: "center", padding: "20px", margin: "10px" }}>
-        <img className="Img-dog" src={Img} style={{ height: "inherit", width: "inherit", objectFit: "contain" }} />
+      <div className="Dogimg">
+        <img className="Img-dog" src={Img} />
       </div>
       <br />
-      <div style={{ display: "flex", alignContent: "center", justifyContent: "center", alignItems: "center", flexDirection: "column", marginTop: "10px" }}>
-        <div style={{ width: "600px" }}>
+      <div className="DogText">
+        <div className="Dogin">
           <h1 style={{ color: "#022840" }}>
             -<span style={{ fontWeight: "1000", textDecoration: "underline" }}>Breed Name:</span> {Dog.name}
           </h1>
